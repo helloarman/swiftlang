6ef7207571080788b6786b91b5c01555660248b4
Arman Rahman <armanrahman0@gmail.com>
2022-11-11 00:46:53 +0600

    Copied notification Added into Button

--- js/app.js
+++ js/app.js
@@ -2,6 +2,15 @@ function output(){
     var json_file_name = document.getElementById('json_file_name').value;
     var text = document.getElementById('text').value;
 
+    // Restore Copied File For JSON
+    var json_copy = document.getElementById('json_copy_btn');
+    json_copy.classList.remove("button-copied");
+    document.getElementById('json_copy_btn').innerHTML = `Copy JSON <i class="fa-regular fa-copy"></i>`;
+    // Restore Copied File For BLADE
+    var json_copy = document.getElementById('blade_copy_btn');
+    json_copy.classList.remove("button-copied");
+    document.getElementById('blade_copy_btn').innerHTML = `Copy Blade <i class="fa-regular fa-copy"></i>`;
+
     var out = text && text.match(
     /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
             .map(s => s.toLowerCase())
@@ -27,6 +36,11 @@ function copyjson(){
     var json_output_text = `"${out}" => "${text}"`
 
     var copyText = json_output_text;
+
+    // After Copy 
+    var json_copy = document.getElementById('json_copy_btn');
+    json_copy.classList.add("button-copied");
+    document.getElementById('json_copy_btn').innerHTML = "JSON Copied!"
     
     navigator.clipboard.writeText(copyText);
 }
@@ -43,6 +57,11 @@ function copyblade(){
     var blade_output_text = `{{ ___('${json_file_name}.${out}') }}`
 
     var copyText = blade_output_text;
+
+    // After Copy 
+    var json_copy = document.getElementById('blade_copy_btn');
+    json_copy.classList.add("button-copied");
+    document.getElementById('blade_copy_btn').innerHTML = "JSON Copied!"
     
     navigator.clipboard.writeText(copyText);
 }
